134d1bc645d4db10ba6e5e7b3fea88785cb33b8e
Kartikey <kartikey@Kartikeys-MacBook-Pro.local>
2020-12-17 23:15:54 +0530

    Added Enum field for devices

--- electronic/models.py
+++ electronic/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from enum import IntEnum
 
 # Create your models here.
+class DeviceType(IntEnum):
+
+    Mobile = 1
+    Laptop = 2
+
+    @classmethod
+    def choices(cls):
+        return [(key.value,key.name)for key in cls]
+
 
 class Product(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,7 +20,11 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images/',blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    device_type = models.CharField(max_length=255, choices=DeviceType.choices(),default=DeviceType.Laptop)
+
 
     def __str__(self):
         return self.name
+
+
 
--- electronic/serializers.py
+++ electronic/serializers.py
@@ -15,5 +15,5 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
     image = serializers.ImageField(max_length=None,allow_empty_file=True,allow_null=True,required=False)
     class Meta:
         model = Product
-        fields = ('id','name','description','image')
+        fields = ('id','name','description','image','device_type')
     
